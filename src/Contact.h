f871876c50f5ce3053ad8a8d8a55beb61af044cb
youssefhatembahig6-alt <youssefhatembahig6@gmail.com>
2026-05-05 15:15:10 +0300

    Add Contact structure

---
+++ src/Contact.h
@@ -0,0 +1,13 @@
+#ifndef CONTACT_H
+#define CONTACT_H
+#include <string>
+using namespace std;
+
+struct Contact {
+    string name;
+    string phone;
+    string email;
+    string address;
+};
+
+#endif
